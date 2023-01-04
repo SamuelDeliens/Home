f782e30b9db06fea8e7b1a4decb5a23b2a1b69c0
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-04 12:49:31 +0100

    Application - Hilt => viewModel

--- Application/app/src/main/java/fr/mastersid/deliens/home/viewModel/HomeViewModel.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/viewModel/HomeViewModel.kt
@@ -1,4 +1,15 @@
 package fr.mastersid.deliens.home.viewModel
 
-class HomeViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import fr.mastersid.deliens.home.backend.HomeUtil
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    state: SavedStateHandle,
+    private val homeUtil : HomeUtil
+) : ViewModel() {
+
 }
