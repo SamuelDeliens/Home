37d0c31c90c631a5c64fffcc95ff5ab3cde544bb
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-04 12:48:13 +0100

    Application - Hilt => view

--- Application/app/src/main/java/fr/mastersid/deliens/home/ui/MainActivity.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package fr.mastersid.deliens.home.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import fr.mastersid.deliens.home.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
