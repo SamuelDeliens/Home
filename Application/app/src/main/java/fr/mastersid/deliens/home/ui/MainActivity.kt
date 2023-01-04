22b9ee96d7e3d958f277e0d13d48c8f936db490b
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-04 12:29:27 +0100

    Application - premier lancemeent

--- Application/app/src/main/java/fr/mastersid/deliens/home/ui/MainActivity.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package fr.mastersid.deliens.home
+package fr.mastersid.deliens.home.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.mastersid.deliens.home.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
