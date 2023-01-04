ec56678587df007f9ab2a8bce201c4ee522f0f46
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-04 12:23:51 +0100

    Application - création application Kotlin

---
+++ Application/app/src/main/java/fr/mastersid/deliens/home/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.mastersid.deliens.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
