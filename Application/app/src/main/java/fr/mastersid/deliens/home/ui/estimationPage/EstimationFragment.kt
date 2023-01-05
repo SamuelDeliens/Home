dee0af05db7b9d4fe5b4e042cec12e28a88e32b3
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-05 13:22:12 +0100

    Application - finalisation design Estimation Page

--- Application/app/src/main/java/fr/mastersid/deliens/home/ui/estimationPage/EstimationFragment.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/ui/estimationPage/EstimationFragment.kt
@@ -1,13 +1,15 @@
-package fr.mastersid.deliens.home.ui
+package fr.mastersid.deliens.home.ui.estimationPage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import fr.mastersid.deliens.home.R
 import fr.mastersid.deliens.home.databinding.FragmentEstimationBinding
-import fr.mastersid.deliens.home.databinding.FragmentListEstimationBinding
+import java.util.*
 
 class EstimationFragment : Fragment() {
 
@@ -19,6 +21,13 @@ class EstimationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentEstimationBinding.inflate(layoutInflater)
+
+        val list_bien = arrayOf<String?>("Maison", "Appartement", "Local", "Terrain", "Autre")
+        val spinner = binding.spinner
+        val adapter: ArrayAdapter<Any?> = ArrayAdapter(requireContext(), R.layout.spinner_list, list_bien)
+        adapter.setDropDownViewResource(R.layout.spinner_list)
+        spinner.adapter
+
         return binding.root
     }
 
