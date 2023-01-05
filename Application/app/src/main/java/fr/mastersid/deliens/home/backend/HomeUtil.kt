c4b29c27b434c13b4369eb93c134b65415c2a82e
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-05 13:43:57 +0100

    Application - modification backend => preparation fonction

--- Application/app/src/main/java/fr/mastersid/deliens/home/backend/HomeUtil.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/backend/HomeUtil.kt
@@ -1,4 +1,6 @@
 package fr.mastersid.deliens.home.backend
 
 interface HomeUtil {
+
+    fun estimation(pieces: Int, surface: Float, region: String): Float
 }
--- Application/app/src/main/java/fr/mastersid/deliens/home/backend/HomeUtilImpl.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/backend/HomeUtilImpl.kt
@@ -3,4 +3,8 @@ package fr.mastersid.deliens.home.backend
 import javax.inject.Inject
 
 class HomeUtilImpl @Inject constructor(): HomeUtil {
+
+    override fun estimation(pieces: Int, surface: Float, region: String): Float {
+        return pieces * surface * 1000
+    }
 }
