1ef965e53a777ff6d9a58c608b61e197374bf809
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-05 15:10:29 +0100

    Application - Ajout parametre dans data

--- Application/app/src/main/java/fr/mastersid/deliens/home/data/EstimationList.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/data/EstimationList.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class EstimationList(val id: Int, val estimation: EstimationResult.Estimation): Parcelable
+data class EstimationList(val id: Int, val estimation: EstimationResult.Estimated): Parcelable
--- Application/app/src/main/java/fr/mastersid/deliens/home/data/EstimationResult.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/data/EstimationResult.kt
@@ -6,10 +6,10 @@ import kotlinx.parcelize.Parcelize
 sealed interface EstimationResult: Parcelable {
 
     @Parcelize
-    data class Estimation(val pieces: Int, val surface: Float, val region: String, val result: Float): EstimationResult
+    data class Estimated(val propertyType: String, val pieces: Int, val surface: Float, val region: String, val result: Float): EstimationResult
 
     @Parcelize
-    data class Failed(val pieces: Int, val surface: Float, val region: String, val error_message: String): EstimationResult
+    data class Failed(val error_message: String): EstimationResult
 
     @Parcelize
     object Empty: EstimationResult
