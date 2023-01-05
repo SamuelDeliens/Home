9d402c59efa19b2b18755a91f0a4094716d2bd76
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-05 18:36:11 +0100

    Application - Ajout des nouvelles vues pour l'annimation carrousel

--- Application/app/src/main/java/fr/mastersid/deliens/home/ui/listEstimationPage/CarouselFragment.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/ui/listEstimationPage/CarouselFragment.kt
@@ -34,35 +34,75 @@ class CarouselFragment: Fragment() {
 
         val motionLayout = binding.motionContainer
 
-        val v1 = motionLayout.get(0)
-        val v2 = motionLayout.get(1)
-        val v3 = motionLayout.get(2)
+        val v1 = motionLayout.get(0) //BLUE
+        val v2 = motionLayout.get(1) //ORANGE
+        val v3 = motionLayout.get(2) //RED
+        val v4 = motionLayout.get(3) //GREEN
+        val v5 = motionLayout.get(4) //PURPLE
 
 
         v1.setOnClickListener {
             if (selectedIndex == 0) return@setOnClickListener
 
-            motionLayout.setTransition(R.id.s2, R.id.s1) //orange to blue transition
+            if (selectedIndex == 1) {
+                motionLayout.setTransition(R.id.s2, R.id.s1) //orange to blue transition
+            } else if (selectedIndex == 4) {
+                motionLayout.setTransition(R.id.s5, R.id.s1) //purple to blue transition
+            }
+
             motionLayout.transitionToEnd()
-            selectedIndex = 0;
+            selectedIndex = 0
         }
+
         v2.setOnClickListener {
             if (selectedIndex == 1) return@setOnClickListener
 
             if (selectedIndex == 2) {
                 motionLayout.setTransition(R.id.s3, R.id.s2)  //red to orange transition
-            } else {
+            } else if (selectedIndex == 0){
                 motionLayout.setTransition(R.id.s1, R.id.s2) //blue to orange transition
             }
             motionLayout.transitionToEnd()
-            selectedIndex = 1;
+            selectedIndex = 1
         }
+
         v3.setOnClickListener {
             if (selectedIndex == 2) return@setOnClickListener
 
-            motionLayout.setTransition(R.id.s2, R.id.s3) //orange to red transition
+            if (selectedIndex == 1) {
+                motionLayout.setTransition(R.id.s2, R.id.s3) //orange to red transition
+            } else if (selectedIndex == 3){
+                motionLayout.setTransition(R.id.s4, R.id.s3) //green to red transition
+            }
+
             motionLayout.transitionToEnd()
-            selectedIndex = 2;
+            selectedIndex = 2
+        }
+
+        v4.setOnClickListener {
+            if (selectedIndex == 3) return@setOnClickListener
+
+            if (selectedIndex == 2) {
+                motionLayout.setTransition(R.id.s3, R.id.s4) //red to green transition
+            } else if (selectedIndex == 4){
+                motionLayout.setTransition(R.id.s5, R.id.s4) //purple to green transition
+            }
+
+            motionLayout.transitionToEnd()
+            selectedIndex = 3
+        }
+
+        v5.setOnClickListener {
+            if (selectedIndex == 4) return@setOnClickListener
+
+            if (selectedIndex == 3) {
+                motionLayout.setTransition(R.id.s4, R.id.s5) //green to purple transition
+            } else if (selectedIndex == 0){
+                motionLayout.setTransition(R.id.s1, R.id.s5) //blue to purple transition
+            }
+
+            motionLayout.transitionToEnd()
+            selectedIndex = 4
         }
     }
 }
