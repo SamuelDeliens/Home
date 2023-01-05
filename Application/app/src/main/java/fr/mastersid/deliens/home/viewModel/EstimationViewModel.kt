9a2d734bb6723f0901ff50c92dcb6528634e0256
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-05 21:03:17 +0100

    Application - EstimationViewModel => gestion exception

--- Application/app/src/main/java/fr/mastersid/deliens/home/viewModel/EstimationViewModel.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/viewModel/EstimationViewModel.kt
@@ -24,8 +24,18 @@ class EstimationViewModel @Inject constructor(
     private val _estimationList : MutableLiveData<List<EstimationResult.Estimated>> = state.getLiveData(STATE_ESTIMATION_LIST, emptyList())
     val estimationList: LiveData<List<EstimationResult.Estimated>> = _estimationList
 
-    fun estimation(propertyType: String, pieces: Int, surface: Float, terrain: Float, region: String) {
-        _resultEstimation.value = EstimationResult.Estimated(propertyType, pieces, surface, region, homeUtil.estimation(propertyType, pieces, surface, terrain, region))
-        _estimationList.value = (_estimationList.value ?: emptyList()) + listOf(_resultEstimation.value as EstimationResult.Estimated)
+    fun estimation(propertyType: String, pieces: Int?, surface: Float?, terrain: Float?, region: String) {
+        try {
+            if (pieces == null || surface == null || terrain == null) {
+                throw IllegalArgumentException("Missing arguments")
+            } else {
+                val estimation = homeUtil.estimation(propertyType, pieces, surface, terrain, region)
+
+                _resultEstimation.value = EstimationResult.Estimated(propertyType, pieces, surface, region, estimation)
+                _estimationList.value = _estimationList.value!! + listOf(EstimationResult.Estimated(propertyType, pieces, surface, region, estimation))
+            }
+        } catch (e: IllegalArgumentException) {
+            _resultEstimation.value = EstimationResult.Failed(e.message!!)
+        }
     }
 }
