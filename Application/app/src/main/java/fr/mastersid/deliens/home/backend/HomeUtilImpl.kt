7f57f92b0166a19fa931d1a42593418e8e81a1fc
SamuelDeliens <samuel.deliens@gmail.com>
2023-01-05 20:38:42 +0100

    Application - backend => finission avec paramètres voulus

--- Application/app/src/main/java/fr/mastersid/deliens/home/backend/HomeUtilImpl.kt
+++ Application/app/src/main/java/fr/mastersid/deliens/home/backend/HomeUtilImpl.kt
@@ -1,5 +1,6 @@
 package fr.mastersid.deliens.home.backend
 
+import android.util.Log
 import javax.inject.Inject
 
 class HomeUtilImpl @Inject constructor(): HomeUtil {
@@ -10,19 +11,19 @@ class HomeUtilImpl @Inject constructor(): HomeUtil {
     )
 
     private val regionMapping = mapOf(
-        "000AB" to 0, "000AC" to 1, "000AD" to 2, "000AE" to 3, "000AH" to 4, "000AI" to 5,
-        "000AK" to 6, "000AO" to 7, "000AP" to 8, "000AR" to 9, "000AS" to 10, "000AT" to 11,
-        "000AV" to 12, "000AW" to 13, "000AX" to 14, "000AY" to 15, "000AZ" to 16,
-        "000BC" to 17, "000BD" to 18, "000BE" to 19, "000BH" to 20, "000BK" to 21,
-        "000BL" to 22, "000BM" to 23, "000BO" to 24, "000BP" to 25, "000BR" to 26,
-        "000BS" to 27, "000BT" to 28, "000BV" to 29, "000BW" to 30
+        "AB" to 0, "AC" to 1, "AD" to 2, "AE" to 3, "AH" to 4, "AI" to 5,
+        "AK" to 6, "AO" to 7, "AP" to 8, "AR" to 9, "AS" to 10, "AT" to 11,
+        "AV" to 12, "AW" to 13, "AX" to 14, "AY" to 15, "AZ" to 16,
+        "BC" to 17, "BD" to 18, "BE" to 19, "BH" to 20, "BK" to 21,
+        "BL" to 22, "BM" to 23, "BO" to 24, "BP" to 25, "BR" to 26,
+        "BS" to 27, "BT" to 28, "BV" to 29, "BW" to 30
     )
 
     override fun estimation(propertyType: String, pieces: Int, surfaceInside: Float, surfaceOutside: Float , region: String): Float {
         val intPropertyType = propertyMapping[propertyType]!!
         val intRegion = regionMapping[region]!!
 
-        return if (propertyType == "Maison") {
+        return if (intPropertyType == 1) {
             estimationMaison(intPropertyType, pieces, surfaceInside, surfaceOutside, intRegion)
         } else {
             estimationAppartement(intPropertyType, pieces, surfaceInside, intRegion)
@@ -31,10 +32,10 @@ class HomeUtilImpl @Inject constructor(): HomeUtil {
 
 
     private fun estimationAppartement(propertyType: Int, pieces: Int, surface: Float, region: Int): Float {
-        return surface * pieces * 1000
+        return surface * pieces * 1000 + region
     }
 
     private fun estimationMaison(propertyType: Int, pieces: Int, surface: Float, terrain: Float, region: Int): Float {
-        return surface * pieces * 1000 + terrain * 1000
+        return surface * pieces * 1000 + terrain * 1000 + region
     }
 }
